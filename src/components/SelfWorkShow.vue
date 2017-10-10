bfbdd14359a63f4cd4251de7e0aad45927df68fc
cjw <86362264@qq.com>
2017-10-10 18:26:10 +0800

    list

--- src/components/SelfWorkShow.vue
+++ src/components/SelfWorkShow.vue
@@ -1,29 +1,44 @@
 <template>
 	<el-row class="selfContainer">
-		
-		<el-col :span="20" :offset="2">
+		<template v-for="item in contentList">
+		<el-col :span="16" :offset="4" class="record">
 			<el-col :span="1" >
 				<img src="../assets/111.jpg" class="head">
 			</el-col>
 			<el-col :span="2" class="">
 				<el-col :span="24">
-					username
+					{{item.userName}}
 				</el-col>
 				<el-col :span="24">
-					time
+					{{item.contentTime}}
 				</el-col>
 			</el-col>
 
 			<el-col :span="24">
-				<template v-for="item in contentList">
+			
+					<el-row>
+						<el-col :span="5">
+							{{item.size}}
+					</el-col>
+					</el-row>
+					
+				    <el-row>
 					<template v-for="itemSon in item.homeWorkLocationPos">
-						<img :src="itemSon.contentUrl" class="imageContent">
+						
+						<el-col :span="item.size">
+						<div class="imageContent">
+							<img :src="itemSon.contentUrl" :class="item.classStyle">
+					    </div>
+						</el-col>
+
 					</template>
-				</template>
+					</el-row>
+			
 			</el-col>
 
 			
 		</el-col>
+	</template>
 		<el-button @click="homeWorkInfo">ssssss</el-button>
 	</el-row>
 </template>
@@ -33,18 +48,43 @@ export default{
 	data(){
 		return {
             contentList:{},
+       
 		}
 	},
 	methods:{
 		homeWorkInfo:function(){
 			getHomeWorkList().then(data=>{
+				
+				for(let i=0;i<data.length;i++){
+					let priClass={};
+					if(data[i].homeWorkLocationPos.length==1){
+                        priClass.maxWidth='500px';
+                        priClass.maxHeigth='500px';
+					}else if(data[i].homeWorkLocationPos.length==2){
+						data[i].size=10;
+						priClass.maxWidth='300px';
+                        priClass.maxHeigth='300px';
+					}
+					else if(data[i].homeWorkLocationPos.length>=3){
+						data[i].size=8;
+						 priClass.maxWidth='300px';
+                        priClass.maxHeigth='300px';
+					}
+					data[i].classStyle=priClass;
+				}
 				this.contentList=data;
+
 			})
 		}
 	}
 }
 </script>
 <style>
+.record{
+	padding: 5px;
+	box-shadow: 0 0 25px #cac6c6;
+}
+
 .head{
 	height: 40px;
 	width: 40px;
@@ -63,8 +103,14 @@ export default{
 	max-width: 100%;
 }
 .imageContent{
-	padding: 5px;
-	height: 20%;
-	width: 20%;
+	overflow: hidden;
+	height: 200px;
+	width: 200px;
+}
+.image{
+	
+	max-width: 300px;
+	max-height: 300px;
+	float: left;
 }
 </style>
